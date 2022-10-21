b2b7f1b39c717d095363e6da40ceec403522d8c2
Thayssa Bernardo <thayssabernardo@gmail.com>
2022-10-21 15:23:16 -0300

    Renomeando Schema Vehicle

--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-const FrameZodSchema = z.object({
+const VehicleSchema = z.object({
   model: z.string().min(3),
   year: z.number().min(1900).max(2022),
   color: z.string().min(3),
@@ -8,6 +8,6 @@ const FrameZodSchema = z.object({
   buyValue: z.number(),
 });
 
-export type IVehicle = z.infer<typeof FrameZodSchema>;
+export type IVehicle = z.infer<typeof VehicleSchema>;
 
-export { FrameZodSchema };
+export { VehicleSchema };
