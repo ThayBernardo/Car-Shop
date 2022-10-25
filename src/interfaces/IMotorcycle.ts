ffbd29756ed46a2e61c9653f347fe751f92950ad
Thayssa Bernardo <thayssabernardo@gmail.com>
2022-10-25 09:45:11 -0300

    Arrumando regras do campo engineCapacity

--- src/interfaces/IMotorcycle.ts
+++ src/interfaces/IMotorcycle.ts
@@ -3,7 +3,7 @@ import { VehicleSchema } from './IVehicle';
 
 const IMotorcycleSchema = VehicleSchema.extend({
   category: z.enum(['Street', 'Custom', 'Trail']),
-  engineCapacity: z.number().max(2500),
+  engineCapacity: z.number().min(1).max(2500),
 });
 
 export type IMotorcycle = z.infer<typeof IMotorcycleSchema>;
